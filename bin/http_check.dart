053d86ba823c8090e5ce591aa2989433e13097a1
Marcus Hanson <mah.dvlpr@gmail.com>
2021-02-17 22:38:15 +0100

    ...

--- bin/http_check.dart
+++ bin/http_check.dart
@@ -3,6 +3,13 @@ import 'package:args/args.dart' as arg;
 import 'package:ansicolor/ansicolor.dart' as color;
 import 'package:http/http.dart' as http;
 
+  // Flow:
+  // Take input (paths to files either relative or absolute)
+  // For each file:
+    // Make request with data from file content
+    // Ignore parts according to file content
+    // Compare to response with expected response (also stored in file)
+
 void main(List<String> arguments) {
 
 }
