fb9b86d47e4cac8abe10d2cf877c85cfe353a759
Marcus Hanson <mah.dvlpr@gmail.com>
2021-02-17 20:28:53 +0100

    init

---
+++ bin/HTTP_Checker.dart
@@ -0,0 +1,5 @@
+import 'package:HTTP_Checker/HTTP_Checker.dart' as HTTP_Checker;
+
+void main(List<String> arguments) {
+  print('Hello world: ${HTTP_Checker.calculate()}!');
+}
---
+++ test/HTTP_Checker_test.dart
@@ -0,0 +1,8 @@
+import 'package:HTTP_Checker/HTTP_Checker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
