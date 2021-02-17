b4d20ffea0a36c1fd5b3644bcde4c738148742e3
Marcus Hanson <mah.dvlpr@gmail.com>
2021-02-17 20:56:41 +0100

    ...

--- bin/HTTP_Checker.dart
+++ bin/HTTP_Checker.dart
@@ -1,5 +1,8 @@
 import 'package:HTTP_Checker/HTTP_Checker.dart' as http_checker;
+import 'package:ansicolor/ansicolor.dart' as color;
 
 void main(List<String> arguments) {
-  print('Hello world: ${http_checker.calculate()}!');
+  // print('Hello world: ${http_checker.calculate()}!');
+  color.AnsiPen pen = color.AnsiPen()..red();
+  print(pen('Hejsan mamma! :D') + "This is normal");
 }
