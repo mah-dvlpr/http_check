8e6e0c1ac2077843a2ddb50dd2d490a8fd7a21dd
Marcus Hanson <mah.dvlpr@gmail.com>
2021-02-18 00:44:56 +0100

    Going to bed... working on the generation bit...

--- bin/http_check.dart
+++ bin/http_check.dart
@@ -1,23 +1,32 @@
-import 'package:args/command_runner.dart';
 import 'dart:io';
+// import 'dart:collection';
+
 import 'package:http_check/http_check.dart' as http_check;
+
+import 'package:args/command_runner.dart';
 import 'package:meta/meta.dart';
 import 'package:args/args.dart' as arg;
 import 'package:http/http.dart' as http;
 import 'package:ansicolor/ansicolor.dart' as color;
 
-  // Flow:
-  // Take input (paths to files either relative or absolute)
-  // For each file:
-    // Make request with data from file content
-    // Ignore parts according to file content
-    // Compare to response with expected response (also stored in file)
+// Flow:
+// Take input (paths to files either relative or absolute)
+// For each file:
+  // Make request with data from file content
+  // Ignore parts according to file content
+  // Compare to response with expected response (also stored in file)
 
-  // TODO:
-  // 1. Create a generator function
-  // 2. Create a request function
-  // 3. Create a ignore function
-  // 4. Create a compare function
+// TODO:
+// 1. Create a generator function
+// 2. Create a request function
+// 3. Create a ignore function
+// 4. Create a compare function
+
+// TODO: Extras
+// 1. Make some parts (such as reading files) asynchronous.
+
+/// Delimiter between request file segments
+const delim = '#####';
 
 void main(List<String> arguments) {
   final parser = arg.ArgParser();
@@ -32,7 +41,7 @@ void main(List<String> arguments) {
     if (flags['continuous']) {
 
     } else if (flags['generate']) {
-      flags.rest.isEmpty ? generateTemplate() : generateTemplate(path: flags.rest);
+      flags.rest.isEmpty ? generateTemplate() : generateTemplate(files: flags.rest);
     } else if (flags['help']) {
 
     }
@@ -50,6 +59,16 @@ void main(List<String> arguments) {
 /// If a file already contains some valid data, make a request to the server as 
 /// requested by the file, ignore patterns as defined by the file, and generate 
 /// (overwrite) the previous expected request data of the file.
-void generateTemplate({List<String> path = const ['generated']}) {
-  
+void generateTemplate({List<String> files = const ['generated']}) {
+  List<String> name, request, ignore, response;
+  for (var file in files) {
+    for (var lines in File(file).readAsLinesSync()) {
+      
+      // if (_.length == 1 && _.toString() == delim) {
+        print(lines);
+      // }
+      // Read each line until a delimiter is encountered
+      // 
+    }
+  }
 }
